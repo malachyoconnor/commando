142e9314bb09ad49315d4be210aba74940d23e10
malachyoconnor <malachyguitar@gmail.com>
2026-09-06 13:47:00 +0100

    First pass implementation

---
+++ commando.cpp
@@ -0,0 +1,112 @@
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <ranges>
+#include <random>
+
+constexpr std::string RED = "\033[31m";
+constexpr std::string GREEN = "\033[32m";
+constexpr std::string BLUE = "\033[34m";
+constexpr std::string RESET = "\033[0m";
+
+constexpr std::string WHITESPACE = " \t\n";
+
+std::string strip_text_after_comment(std::string &&line) {
+   auto first_char = line.find_first_of("//");
+   if (first_char == std::string::npos) {
+      return std::move(line);
+   }
+
+   line.erase(first_char);
+   return std::move(line);
+}
+
+std::string strip_text_after_comment(const std::string &line) = delete;
+
+std::vector<std::pair<std::string, std::vector<std::string> > > get_commands_and_alt_text(std::ifstream &file) {
+   std::vector<std::pair<std::string, std::vector<std::string> > > result_vec{};
+   std::string line;
+   std::streampos prev_line_pos = file.tellg();
+
+   while (std::getline(file, line)) {
+      line = strip_text_after_comment(std::move(line));
+
+      auto expected_dash_loc = line.find_first_not_of(WHITESPACE);
+      if (expected_dash_loc == std::string::npos || line[expected_dash_loc] != '-') continue;
+
+      prev_line_pos = file.tellg();
+
+      auto command_start_loc = line.find_first_not_of(WHITESPACE, expected_dash_loc + 1);
+      if (command_start_loc == std::string::npos) continue;
+
+      std::string command = line.substr(command_start_loc);
+      std::vector<std::string> description{};
+
+      while (std::getline(file, line)) {
+         line = strip_text_after_comment(std::move(line));
+
+         auto non_whitespace_loc = line.find_first_not_of(WHITESPACE);
+         if (non_whitespace_loc != std::string::npos && line[non_whitespace_loc] == '-') {
+            file.seekg(prev_line_pos);
+            break;
+         }
+         if (non_whitespace_loc == std::string::npos) continue;
+
+         description.push_back(line);
+      }
+
+      result_vec.emplace_back(command, description);
+   }
+
+   return result_vec;
+}
+
+void print_chosen_command_and_description(std::string &command, std::vector<std::string> &description) {
+   std::cout << std::format("{}{}{}", GREEN, command, RESET) << std::endl;
+
+   for (size_t i{}; i < description.size(); i++) {
+      auto &line = description[i];
+      // Very inefficient, but easy (:
+      for (size_t comm_loc = line.find(command, 0); comm_loc != std::string::npos; comm_loc = line.find(command, comm_loc)) {
+         line.insert(comm_loc, GREEN);
+         comm_loc += GREEN.size() + command.size();
+         line.insert(comm_loc, RESET);
+         comm_loc += RESET.size();
+      }
+
+      std::cout << "   " << line;
+      if (i != description.size() - 1) {
+         std::cout  << std::endl;
+      }
+   }
+}
+
+int main(int argc, char **argv) {
+   if (argc <= 1) {
+      std::cerr << "No commands file passed as argument!" << std::endl;
+      return 1;
+   }
+   if (argc >= 3) {
+      std::cerr << std::format("Too many commands passed to 'commando': {}", argc) << std::endl;
+      return 1;
+   }
+
+   std::filesystem::path commands_file = argv[1];
+
+   std::ifstream file(commands_file);
+
+   auto result_vec = get_commands_and_alt_text(file);
+
+   std::mt19937 gen{std::random_device{}()};
+   std::uniform_int_distribution<int> dist(0, static_cast<int>(result_vec.size()) - 1);
+
+   auto random_num = dist(gen);
+   
+   auto &[command, description] = result_vec[random_num];
+
+   print_chosen_command_and_description(command, description);
+
+   return 0;
+}
---
+++ install.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/bash
+
+INSTALL_SCRIPT_FILE_PATH="$(realpath "${BASH_SOURCE[0]}")"
+REPO_PATH="$(dirname "$INSTALL_SCRIPT_FILE_PATH")"
+
+# Colour escape codes
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+GREY='\033[2;37m'
+RED='\033[0;31m'
+RESET='\033[0m'
+
+function build_commando() {
+    compile_string="g++ ./commando.cpp -o commando"
+    echo "$compile_string"
+
+    # shellcheck disable=SC2046
+    return $($compile_string)
+}
+
+if build_commando; then
+    echo -e "${GREEN}Built successfully!${RESET}"
+else
+    echo -e "${RED}Build failed${RESET}"
+    exit 1
+fi
+
+BASHRC_PATH=~/.bashrc
+#BASHRC_PATH=~/TEST_FILE
+
+if [ ! -f "$BASHRC_PATH" ]; then
+    echo "${RED}No .bashrc found!${RESET}"
+    exit 1
+fi
+
+echo -e -n "${BLUE}Do you want to update $BASHRC_PATH? (y/n):${RESET} "
+read choice
+# Default to yes if they press enter
+choice="${choice:-y}"
+case "$choice" in 
+    y|Y ) 
+        ;;
+    n|N ) 
+        echo -e "${GREEN}Finished.${RESET}"
+        exit 1
+        ;;
+    * ) 
+        echo -e "${RED}Invalid choice.${RESET}"
+        exit 1
+        ;;
+esac
+
+if grep -q "/commando" "$BASHRC_PATH"; then
+    echo -e -n "${RED}found 'commando' in '$BASHRC_PATH'.${RESET} ${BLUE}Are you sure you want to continue? (y/n)${RESET}: " 
+    read choice
+    choice="${choice:-y}"
+    case "$choice" in 
+        y|Y ) 
+            ;;
+        n|N ) 
+            echo "Finished."
+            exit 1
+            ;;
+        * ) 
+            echo "Invalid choice."
+            exit 1
+            ;;
+    esac
+fi 
+
+# shellcheck disable=SC2034
+COMMANDS_TEXT_FILE_PATH="$REPO_PATH/commands.txt"
+# Multiline command string
+COMMAND_TO_APPEND_TO_BASHRC=$(
+    cat <<EOF
+
+# commando: Print a random script you want to remember on shell start.
+$REPO_PATH/commando "$COMMANDS_TEXT_FILE_PATH"
+EOF
+)
+
+echo "$COMMAND_TO_APPEND_TO_BASHRC" >> "$BASHRC_PATH"
+echo -e "${GREEN}$BASHRC_PATH updated${RESET}"
+
+
+echo "=========================UPDATED WITH=================================="
+echo -e -n "${GREY}"
+tail -3  $BASHRC_PATH | cat
+echo -e -n "${RESET}"
+echo "======================================================================="
